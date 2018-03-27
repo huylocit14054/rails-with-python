472010c92fb29fd8bd4470127dc1a8efc3d3fc6a
Le Dinh Huy Loc <sasuke1996vn@gmail.com>
2018-03-27 21:17:57 +0700

    fix route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root :to => 'factorial#new'
-  post 'factorial' , to: 'factorial#calculate_factorial' 
-  get 'factorial' , to: 'factorial#new' 
+  post 'factorial_loc' , to: 'factorial#calculate_factorial' 
+  get 'factorial_loc' , to: 'factorial#new' 
 end
