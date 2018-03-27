8b88ae53d5b96550b0b62ac20dd2faa8796f662a
Le Dinh Huy Loc <sasuke1996vn@gmail.com>
2018-03-27 21:11:50 +0700

    add skip_before_action

--- app/controllers/factorial_controller.rb
+++ app/controllers/factorial_controller.rb
@@ -1,5 +1,5 @@
 class FactorialController < ApplicationController
-    skip_before_filter :verify_authenticity_token  
+    skip_before_action :verify_authenticity_token
     #display input form
     def new
         @result = 0 
